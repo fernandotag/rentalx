8ea715915837d934b3d10d151d9f00bc200c6bbc
Fernando Tagliaferro <4048111+fernandotag@users.noreply.github.com>
2023-02-16 16:26:14 -0300

    chore: config eslint

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,19 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.listen(3333, () => console.log('Server is running'));
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  return res.json({ message: "Hello world" });
+});
+
+app.post("/courses", (req, res) => {
+  const { name } = req.body;
+
+  return res.json({ name });
+});
+
+app.listen(3333, () => {
+  console.log("Server is running");
+});
