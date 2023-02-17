69cd242e9a859c45e3b0bbc1ab33f22ead983bbd
Fernando Tagliaferro <4048111+fernandotag@users.noreply.github.com>
2023-02-17 14:51:21 -0300

    refactor: create category registration use case

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -0,0 +1,17 @@
+import { type Response, type Request } from "express";
+
+import { type CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+class CreateCategoryController {
+  constructor(private readonly createCategoryUseCase: CreateCategoryUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    const { name, description } = request.body;
+
+    this.createCategoryUseCase.execute({ name, description });
+
+    return response.status(201).send();
+  }
+}
+
+export { CreateCategoryController };
